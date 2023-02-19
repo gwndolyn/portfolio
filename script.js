c0e58dd5265b63032e195ff0b50429ec6142442d
gwndolyn <s10223182@connect.edu.sg>
2023-02-19 14:45:01 +0800

    .

--- script.js
+++ script.js
@@ -77,4 +77,36 @@ window.onload = function() {
 };
 
 
+// $(document).on("scroll", function() {
+ // var pageTop = $(document).scrollTop();
+ // var pageBottom = pageTop + $(window).height();
+ // var tags = $(".tag");
 
+ // for (var i = 0; i < tags.length; i++) {
+ //   var tag = tags[i];
+
+ //   if ($(tag).position().top < pageBottom) {
+ //     $(tag).addClass("visible");
+  //  } else {
+ //     $(tag).removeClass("visible");
+ //   }
+//  }
+//});
+
+function tag() {
+  var tags = document.querySelectorAll(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = tags[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      tags[i].classList.add("visible");
+    } else {
+      tags[i].classList.remove("visible");
+    }
+  }
+}
+
+window.addEventListener("scroll", tag);
